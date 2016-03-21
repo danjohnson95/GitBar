8a0e2777b4ec7a6c5d93477b445576db72deda08
Dan <dan@danj.eu>
2016-03-21 21:03:48 +0000

    Initial Commit

---
+++ src/GitbarServiceProvider.php
@@ -0,0 +1,17 @@
+<?
+
+namespace Danj\Gitbar;
+
+class GitbarServiceProvider extends \Illuminate\Support\ServiceProvider{
+
+	public function register(){
+		//dd('yo');
+	}
+	
+	public function boot(){
+
+
+	}
+
+
+}
