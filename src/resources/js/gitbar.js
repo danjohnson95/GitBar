fab51c959d170175ba6e9daeafcd0812ea490310
Dan <dan@danj.eu>
2016-03-23 20:07:47 +0000

    CSS and JS Tweaks
    Loading spinners now display for both the button and the current branch
    on page load.
    All required HTML is injected to the page via JS

--- src/resources/js/gitbar.js
+++ src/resources/js/gitbar.js
@@ -7,14 +7,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var gitbarCurrent = document.createElement('div');
 	var gitbarBranches = document.createElement('div');
 	var gitbarButton = document.createElement('button');
+	var loading = document.createElement('div');
+	var loadingSpan = document.createElement('span');
+	var gitbarStrongBranch = document.createElement('strong');
+	var branchSelect = document.createElement('select');
+
+	loadingSpan.className = "load";
+	loading.className = "load";
+
+	gitbarStrongBranch.appendChild(loadingSpan);
+
 	gitbar.id = "gitbar";
 	gitbarCurrent.className = "gitbar-current";
 	gitbarBranches.className = "gitbar-branches";
 	gitbarButton.id = "checkout-branch";
-	gitbarButton.innerHTML = "test";
+	gitbarButton.appendChild(loading);
 
-	gitbarCurrent.innerHTML =	"<div class='gitbar-current'>Current Branch <strong></strong></div>";
+	gitbarCurrent.innerHTML =	"Current Branch";
+	gitbarCurrent.appendChild(gitbarStrongBranch);
 	gitbarBranches.innerHTML = "Switch branch";
+	gitbarBranches.appendChild(branchSelect);
 	gitbarBranches.appendChild(gitbarButton);
 
 	gitbar.appendChild(gitbarCurrent);
