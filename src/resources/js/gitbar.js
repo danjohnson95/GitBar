d8bb97d4f3628d4a2afcc38daa6607a9f55850a5
Dan <dan@danj.eu>
2016-03-23 20:19:31 +0000

    More JS tweaks
    Javascript is now much tidier for injecting the HTML to the page

--- src/resources/js/gitbar.js
+++ src/resources/js/gitbar.js
@@ -1,37 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-	//Document is ready.
-	//Here's the HTML.
+	var html = '<div id="gitbar">' + 
+		 		'<div class="gitbar-current">' +
+		 			'Current Branch' +
+	 				'<strong>' +
+	 					'<span class="load"></span>' +
+	 				'</strong>' +
+ 				'</div>' +
+ 				'<div class="gitbar-branches">' +
+ 					'Switch Branch' +
+ 					'<select></select>' +
+ 					'<button id="checkout-branch">' +
+ 						'<div class="load"></div>' +
+ 					'</button>' +
+ 				'</div>' +
+ 			'</div>';
 
-	var gitbar = document.createElement('div');
-	var gitbarCurrent = document.createElement('div');
-	var gitbarBranches = document.createElement('div');
-	var gitbarButton = document.createElement('button');
-	var loading = document.createElement('div');
-	var loadingSpan = document.createElement('span');
-	var gitbarStrongBranch = document.createElement('strong');
-	var branchSelect = document.createElement('select');
-
-	loadingSpan.className = "load";
-	loading.className = "load";
-
-	gitbarStrongBranch.appendChild(loadingSpan);
-
-	gitbar.id = "gitbar";
-	gitbarCurrent.className = "gitbar-current";
-	gitbarBranches.className = "gitbar-branches";
-	gitbarButton.id = "checkout-branch";
-	gitbarButton.appendChild(loading);
-
-	gitbarCurrent.innerHTML =	"Current Branch";
-	gitbarCurrent.appendChild(gitbarStrongBranch);
-	gitbarBranches.innerHTML = "Switch branch";
-	gitbarBranches.appendChild(branchSelect);
-	gitbarBranches.appendChild(gitbarButton);
-
-	gitbar.appendChild(gitbarCurrent);
-	gitbar.appendChild(gitbarBranches);
-
-	document.body.appendChild(gitbar);
+	document.body.innerHTML += html;
 
 });
